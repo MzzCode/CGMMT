f90411462b36b4642ea9df4b0fb0d94baa5a9e18
Mohd Zeeshan Zafar <99405704+MzzCode@users.noreply.github.com>
2022-02-10 19:15:55 +0530

    Create Smiley.cpp

---
+++ Smiley.cpp
@@ -0,0 +1,13 @@
+#include<graphics.h> #include <stdio.h> 
+int main ()
+{
+int gd = DETECT, gm;
+initgraph (&gd, &gm, "C:\\TURBOC3\\BGI");
+ circle (200,200,30);
+circle(190,190,5);
+circle(210,190,5);
+arc(200,210,180,360,10);
+getch();
+ closegraph(); 
+return 0;
+}
